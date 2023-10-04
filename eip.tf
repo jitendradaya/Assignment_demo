ce85b5308f781549600e0c829876a54695aa7832
Jitendra Daya <jitendra.daya@gmail.com>
2023-10-04 19:25:40 +0530

    Added terraform code

--- eip.tf
+++ eip.tf
@@ -1,3 +1,3 @@
 resource "aws_eip" "myeip" {
-  vpc = true
+  domain = "vpc"
 }
--- keypair.tf
+++ keypair.tf
@@ -1,5 +1,5 @@
 # Create keypair for ec2 instance 
 resource "aws_key_pair" "keypair" {
   key_name   = "access"                              # Change this to your desired key pair name
-  public_key = file("/home/jitendra/.ssh/id_rsa.pub") #Replace with your keypair 
+  public_key = file("C:\\Users\\jitendra.daya_infobe\\.ssh\\id_rsa.pub") #Replace with your keypair 
 }
